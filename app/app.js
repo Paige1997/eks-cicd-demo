86664d62f350015457f472ad92f9196f8292583b
Alanna-Paige Dixon <alannapaige.dixon@particularpresence.com>
2025-11-15 08:18:23 -0500

    edit message

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js EKS CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from Node.js EKS CI/CD'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
